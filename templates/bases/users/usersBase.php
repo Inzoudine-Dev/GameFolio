e078c588fe4fffe196c3ad519aafb90dd6ec0840
Maham <mahamoud-inzoudine@hotmail.fr>
2024-09-07 22:36:16 +0200

    commite5

--- templates/bases/users/usersBase.php
+++ templates/bases/users/usersBase.php
@@ -8,18 +8,32 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php echo $titre_page ?></title>
-        <link rel="shortcut icon" href="/GestionPortefeuilleClients/public/images/logoGestionClient.png" />
+        <link rel="shortcut icon" href="/GameFolio/public/images/logo_jeuxVideos.png" />
         <link rel="stylesheet" type="text/css" href="/GameFolio/public/styleCss/users/usersBase.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="/GameFolio/public/styleCss/users/viewHome.css" media="all" />
 
     </head>
 
     <body>
 
         <header class="header_class">
-            <div class="header_content">
-                <a href="/GestionPortefeuilleClients/accueil"><img src="/GestionPortefeuilleClients/public/images/logoGestionClient.png" alt="logo" class="logo_class"/></a>
-                <h1>Bienvenue sur l'application de gestion des clients</h1>
-            </div>
+
+            <nav class="nav_class">
+
+                <ul class="ul_class">
+
+                    <a href="/GameFolio/home" class="lien_logo_class"><img src="/GameFolio/public/images/logo_jeuxVideos.png" alt="logo" class="logo_class"/></a>
+                    <li class="li_class">menue 1</li>
+                    <li class="li_class">menue 2</li>
+                    <li class="li_class">menue 3</li>
+                    <li class="li_class">menue 4</li>
+                    <li class="li_class">menue 5</li>
+                    <li class="li_class" id="six_id">menue 6</li>
+                    <a href="/GameFolio/home" class="lien_iconeConnexion_class"><img src="/GameFolio/public/images/icone_connexion.png" alt="logo" class="icone_connexion"/></a>
+
+                </ul>
+            </nav>
+
         </header>
 
         <main class="main_class">
