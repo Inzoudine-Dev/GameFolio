a6a889d9672f20f474a260434b8da1bb005cfa75
Maham <mahamoud-inzoudine@hotmail.fr>
2024-09-07 22:43:22 +0200

    commite6

--- templates/bases/users/usersBase.php
+++ templates/bases/users/usersBase.php
@@ -23,12 +23,12 @@
                 <ul class="ul_class">
 
                     <a href="/GameFolio/home" class="lien_logo_class"><img src="/GameFolio/public/images/logo_jeuxVideos.png" alt="logo" class="logo_class"/></a>
-                    <li class="li_class">menue 1</li>
-                    <li class="li_class">menue 2</li>
-                    <li class="li_class">menue 3</li>
-                    <li class="li_class">menue 4</li>
-                    <li class="li_class">menue 5</li>
-                    <li class="li_class" id="six_id">menue 6</li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">menue 1</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">menue 2</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">menue 3</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">menue 4</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">menue 5</a></li>
+                    <li class="li_class" id="six_id"><a href="/GameFolio/home" class="">menue 6</a></li>
                     <a href="/GameFolio/home" class="lien_iconeConnexion_class"><img src="/GameFolio/public/images/icone_connexion.png" alt="logo" class="icone_connexion"/></a>
 
                 </ul>
