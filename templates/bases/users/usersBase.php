861c207d85d50c938f4897b7d3e786fdde01ff6b
Maham <mahamoud-inzoudine@hotmail.fr>
2024-09-09 21:09:42 +0200

    commite19

--- templates/bases/users/usersBase.php
+++ templates/bases/users/usersBase.php
@@ -23,12 +23,12 @@
                 <ul class="ul_class">
 
                     <a href="/GameFolio/home" class="lien_logo_class"><img src="/GameFolio/public/images/logo_jeuxVideos.png" alt="logo" class="logo_class"/></a>
-                    <li class="li_class"><a href="/GameFolio/home" class="">menue 1</a></li>
-                    <li class="li_class"><a href="/GameFolio/home" class="">menue 2</a></li>
-                    <li class="li_class"><a href="/GameFolio/home" class="">menue 3</a></li>
-                    <li class="li_class"><a href="/GameFolio/home" class="">menue 4</a></li>
-                    <li class="li_class"><a href="/GameFolio/home" class="">menue 5</a></li>
-                    <li class="li_class" id="six_id"><a href="/GameFolio/home" class="">menue 6</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">ACCUEIL</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">JEUX VIDEOS</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">OFFRES</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">NOUS</a></li>
+                    <li class="li_class"><a href="/GameFolio/home" class="">NOS BOUTIQUES</a></li>
+                    <li class="li_class" id="six_id"><a href="/GameFolio/home" class="">CONTACTS</a></li>
                     <a href="/GameFolio/home" class="lien_iconeConnexion_class"><img src="/GameFolio/public/images/icone_connexion.png" alt="logo" class="icone_connexion"/></a>
 
                 </ul>
--- templates/views/users/viewHome.php
+++ templates/views/users/viewHome.php
@@ -20,6 +20,77 @@
 
         </div>
 
+        <hr>
+
+    <h1 class="titre_jeuxCourses_class">Jeux de courses</h1>
+
+    <div class="games_race_class">
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_imageGame1_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_imageGame1_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_imageGame1_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_imageGame1_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+    </div>
+
+    <hr>
+
+    <h1 class="titre_jeuxCombat_class">Jeux de combat</h1>
+
+    <div class="games_race_class">
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_iconeConnexion_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_iconeConnexion_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_iconeConnexion_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+
+        <article class="game_class">
+
+            <a href="/GameFolio/home" class="lien_iconeConnexion_class"> <img src="/GameFolio/public/images/imagesGames/imageGame1.jpg" alt="logo" class="image_game_class"/> </a>
+
+        </article>
+
+    </div>
+
+
 
 
 </section>
