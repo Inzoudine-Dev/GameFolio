43db4465aa7bfd752e922f625e24562dc9d8af62
Maham <mahamoud-inzoudine@hotmail.fr>
2024-12-12 02:13:26 +0100

    commite89

--- templates/views/layout/users/usersFooter.php
+++ templates/views/layout/users/usersFooter.php
@@ -2,6 +2,9 @@
 
 <div class="footer_content_class">
 
-    <h1>hello footer !!</h1>
+
+    <a href=""><h4>PLAN DU SITE</h4></a>
+    <a href=""><h4>CONFIDENTILITE</h4></a>
+    <a href=""><h4>DEVELOPPEUR: INZOUDINE</h4></a>
 
 </div>
